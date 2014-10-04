bcf271460c415e4216c27eb5a2e8a2e408cb1cab
Rajarshi Sarkar <rajarshi1397.11@bitmesra.ac.in>
2014-10-04 14:41:04 +0530

    Delete WOW_SQR_short.c

--- Sphere Online Judge/WOW_SQR_short.c
+++
@@ -1,1 +0,0 @@
-long long T,n,k,y,x,i,X,Y,a;main(){scanf("%u",&T);while(T--){scanf("%ld",&n);a=0;for(i=2;i<=n/i;++i){k=n/(i*i);y=k*i*i;x=k*(i-1)*(i-1);if(x*y>a)a=x*y,X=x,Y=y;}printf("%lld*%d\n",X,Y);}}
