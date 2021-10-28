77631f642a682e8e5c3df76530f61d670c9dca6c
williams-sandjaya <75717742+williams-sandjaya@users.noreply.github.com>
2021-10-28 12:04:38 +0700

    Update JOKER3.cpp
    I'm requesting a pull request because from what i saw the algorithmy you write in old code is O(N^3) and in spoj it gives TLE while mine give 0.43 in O(N) solution.Thank You.

--- Sphere Online Judge/JOKER3.cpp
+++ Sphere Online Judge/JOKER3.cpp
@@ -1,33 +1,31 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#include <bits/stdc++.h>
 using namespace std;
- 
-int main() 
-{
-	int test,n;
+int test,n,arr[1000000];
+int main(){
 	scanf("%d",&test);
-	while(test--)
-	{
-		scanf("%d", &n);
-		int a[n]; 
-	    FOR(i,1,n) 
-	        scanf("%d", &a[i]);
- 
-	    int res = 0;
-	    FOR(i,1,n) 
-	        FOR(j,i,n) 
-	        {
-	            int now = 0;
-	            FOR(k,1,n) 
-	            {
-	                if (k >= i && k <= j) now += 1 - a[k];
-	                else now += a[k];
-	            }
-	            res = max(res, now);
-	        }
-	    cout << res << endl;
+	while(test--){
+		int knive = 0;
+		scanf("%d",&n);
+		arr[n+5];
+		int dp[n+5];
+	    for(int i=1;i<=n;i++){
+			scanf("%d",&arr[i]);
+	        if(arr[i] == 1){
+	        	knive++;
+				arr[i] = -1;
+	        }else if(arr[i] == 0){
+				arr[i] = 1;
+			}
+			 
+	    }
+		if(arr[1]==1) dp[1] = 1;
+		else if(arr[1] == -1)dp[1] = -1;
+	    int maxx = dp[1];
+	    for(int i=2;i<=n;i++){
+	    	dp[i] = max(arr[i]+dp[i-1],arr[i]);
+	    	maxx = max(maxx,dp[i]);
+	    }
+	    printf("%d\n",maxx+knive);
 	}
+	return 0;
 }
